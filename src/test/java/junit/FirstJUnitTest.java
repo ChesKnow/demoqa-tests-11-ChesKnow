9348bdb8a7818274ee947ab59cbbf66e028136cc
Roman Chesanov <roman.chesanov>
2022-02-10 23:57:50 +0300

    Change test file

--- src/test/java/junit/FirstJUnitTest.java
+++ src/test/java/junit/FirstJUnitTest.java
@@ -1,11 +1,30 @@
 package junit;
 
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FirstJUnitTest {
+
+    @BeforeEach
+    void openBrowser() {
+        Selenide.open("https://ya.ru");
+    }
+
+    @AfterEach
+    void closeBrowser() {
+        Selenide.closeWebDriver();
+    }
+
     @Test
-    void simpleTest() {
-        Assertions.assertTrue(2<3);
+    void textTest() {
+        // search for text
+    }
+
+    @Test
+    void videoTest() {
+        // search for video
     }
 }
